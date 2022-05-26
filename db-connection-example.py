0d18ab1b2d2f4a321b8dc16b4331a29651708c4e
Jorge Nava de la Peña <jorge.pena@ibm.com>
2022-05-26 16:52:09 -0500

    First commit

---
+++ db-connection-example.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+import datetime
+
+DB_CONNECTION_STRING = "mongodb+srv://jesus:cisco@arquitecturadesoftware.ddie5.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(DB_CONNECTION_STRING)
+
+db = client.arquitecturadesoftware
+empresa = db.empresa
+clientes = db.clientes
+empleados = db.empleados
+
+# CREATE OBJECT TO USE IN 'INSERT'
+""" 
+empresa_1 = {
+    "id": 1,
+    "nombre": "Big Dick Destroyer"
+} 
+"""
+# DO THE ACTUAL INSERTION IN DB
+# empresa.insert_one(empresa_1)
+
+# SEARCH IN TABLE BY SOME ATTRIBUTES IN THE OBJECT OF THE CLASS
+print(empresa.find_one({ "nombre": "Big Dick Destroyer" }))
