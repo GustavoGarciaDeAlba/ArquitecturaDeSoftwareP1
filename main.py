3bb7d72b4a819692e6f98668e2abcb593a8fa9a5
Gustavo Garcia de Alba Espinosa <gustavogarciadealba@hotmail.com>
2022-06-02 18:23:11 -0500

    Merge branch 'master' of https://github.com/GustavoGarciaDeAlba/ArquitecturaDeSoftwareP1

---
+++ main.py
@@ -0,0 +1,27 @@
+if __name__ == "__main__":
+    decision = ""
+    while True:
+        print("""============Menu===================\n
+1. Ingresar informacion Empresa
+2. Ingresar informacion Cliente
+3. Ingresar informacion Empleado
+4. Mostrar informacion Empresa
+5. Mostrar informacion Cliente
+6. Mostrar informacion Empleado\n""")
+        if decision == "1":
+            pass
+        elif decision == "2":
+            pass
+
+        elif decision == "3":
+            pass
+
+        elif decision == "4":
+            pass
+            
+        elif decision == "5":
+            pass
+
+        elif decision == "6":
+            pass
+
